03ab23c54a3bab31dcfb09e6357045ce8eae3d30
jjJkJjkkk <104215882+jjJkJjkkk@users.noreply.github.com>
2022-04-22 21:57:29 +0300

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	//зміна коду
+	double l, T;
+	const double PI = 3.14159;
+	const double g = 9.8;
+	cout << "T = 2*PI*((l/g)^(0.5))" << endl;
+	cout << "g = 9.8" << endl;
+	cout << "PI = 3.14159" << endl;
+	cout << "Enter the length (l): ";
+	cin >> l;
+	if (l < 0) {
+		cout << "The number entered cannot be a length, enter a positive number: ";
+		cin >> l;
+		T = (2 * (PI) * (sqrt(l / g)));
+		cout << "T = " << T << endl;
+	}
+	else {T = (2 * (PI) * (sqrt(l / g)));
+		cout << "T = " << T << endl;
+	}
+	return 0;
+}
